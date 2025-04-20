2dbdc5e271f7d65c834ab9e312d660313cfb1d64
Kunal Tiwari <kunalt@cs.utexas.edu>
2025-04-20 16:19:46 -0500

    Added more CUDA code

--- n-body-simulation.cu
+++ n-body-simulation.cu
@@ -11,40 +11,34 @@ typedef struct{
     float mass;
 } Body;
 
+__global__ void update(Body* bodies, int n, float dt){
+    for (int i = 0; i < n; i++) {
+        for (int j = i + 1; j < n; j++) {
+            float2 p1 = bodies[i].pos;
+            float m1 = bodies[i].mass;
+            float2 p2 = bodies[j].pos;
+            float m2 = bodies[j].mass;
 
-typedef struct Simulation{
+            float dx = bodies[j].pos.x - bodies[i].pos.x;
+            float dy = bodies[j].pos.y - bodies[i].pos.y;
+            float dist = sqrt(dx * dx + dy * dy);
 
-    int n;
-    Body[] bodies;
+            float2 r = make_float2(dx / dist, dy / dist);
 
-    __global__ update() {
-        for (int i = 0; i < n; i++) {
-            for (int j = i + 1; j < n; j++) {
-                float2 p1 = bodies[i].pos;
-                float m1 = bodies[i].mass;
-                float2 p2 = bodies[j].pos;
-                float m2 = bodies[j].mass;
+            float grav = r / (dist * dist * dist);
 
-                float dx = bodies[j].pos.x - bodies[i].pos.x;
-                float dy = bodies[j].pos.y - bodies[i].pos.y;
-                float dist = sqrt(dx * dx + dy * dy);
-
-                float2 r = make_float2(dx / dist, dy / dist);
-
-                float grav = r / (dist * dist * dist);
-
-                bodies[i].acc += m2 * grav;
-                bodies[j].acc -= m1 * grav;
-            }
+            bodies[i].acc += m2 * grav;
+            bodies[j].acc -= m1 * grav;
         }
     }
-
-
 }
 
 //Host Code
 int main() {
     const int N = 1024; // number of bodies
+    const float  dt     = 0.01f;
+    const int    steps  = 1000;
+
     std::vector<Body> hostP(N);
     for(int i = 0; i < N; ++i){
         hostP[i].pos = make_float2(rand()/float(RAND_MAX), rand()/float(RAND_MAX));
@@ -53,5 +47,23 @@ int main() {
         hostP[i].mass = 1.0f;
     }
 
+    Body* devP;
+    cudaMalloc(&devP, N * sizeof(Body));
+    cudaMemcpy(devP, hostP.data(), N * sizeof(Body), cudaMemcpyHostToDevice);
 
+    int threadsPerBlock = 256;
+    int blocks = (N + threadsPerBlock - 1) / threadsPerBlock;
+
+    for (int t = 0; t < steps; ++t) {
+        update<<<blocks, threadsPerBlock>>>(devP, N, dt);
+        cudaDeviceSynchronize();
+    }
+
+    std::cout << "Body[0] pos = ("
+        << hostP[0].pos.x << ", "
+        << hostP[0].pos.y << ")\n";
+
+
+    cudaFree(devP);
+    return 0;
 }
